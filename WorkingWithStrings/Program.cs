242c10a92e54d78f56024ec6a0fb40103f21e79e
Jim Flores <jimflores5@gmail.com>
2017-11-03 09:56:07 -0500

    Added a list to collect the initial set of strings.

--- WorkingWithStrings/Program.cs
+++ WorkingWithStrings/Program.cs
@@ -10,23 +10,29 @@ namespace WorkingWithStrings
         static void Main(string[] args)
         {
             //First, let's format some strings.
-            string myString = "What can we do with \"Rutabagas\"?";  // The "\" allows us to use double quotes inside the string.
-            string myString2 = "What if we want \nto break to a new line?";  // \n moves to a new line.
-            string myString3 = @"What if we want to print the backslash (\) character?";  // @ tells C# to treat \ as a character.
-            string myString4 = String.Format("{1} = {0}","A","B");  // The {} are tags that get replaced by "A" and "B".
-            string myString5 = String.Format("{0:C}", 5.63);  // The tag will be formatted to display $ and cents.
-            string myString6 = string.Format("{0:N}", 12345678905);  // The tag will be formatted to display commas and a decimal point.
-            string myString7 = string.Format("{0:P}", 0.0563);  // The tag will be formatted to display a percentage.
-            string myString8 = string.Format("Phone number: {0:(###) ###-####}", 1234567890);  // The tag will be custom formatted FROM RIGHT TO LEFT.
+            List<string> messages = new List<string>();
 
-            Console.WriteLine(myString);
-            Console.WriteLine(myString2);
-            Console.WriteLine(myString3);
-            Console.WriteLine(myString4);
-            Console.WriteLine(myString5);
-            Console.WriteLine(myString6);
-            Console.WriteLine(myString7);
-            Console.WriteLine(myString8);
+            string myString = "What can we do with \"Rutabagas\"?";  // The "\" allows us to use double quotes inside the string.
+            messages.Add(myString);
+            string myString2 = "What if we want \nto break to a new line?";  // \n moves to a new line.
+            messages.Add(myString2);
+            string myString3 = @"What if we want to print the backslash (\) character?";  // @ tells C# to treat \ as a character.
+            messages.Add(myString3);
+            string myString4 = String.Format("{1} = {0}","A","B");  // The {} are tags that get replaced by "A" and "B".
+            messages.Add(myString4);
+            string myString5 = String.Format("{0:C}", 5.63);  // The tag will be formatted to display $ and cents.
+            messages.Add(myString5);
+            string myString6 = string.Format("{0:N}", 12345678905);  // The tag will be formatted to display commas and a decimal point.
+            messages.Add(myString6);
+            string myString7 = string.Format("{0:P}", 0.0563);  // The tag will be formatted to display a percentage.
+            messages.Add(myString7);
+            string myString8 = string.Format("Phone number: {0:(###) ###-####}", 1234567890);  // The tag will be custom formatted FROM RIGHT TO LEFT.
+            messages.Add(myString8);
+
+            foreach (string message in messages)
+            {
+                Console.WriteLine(message);
+            }
             Console.ReadLine();
 
             //Next, let's manipulate strings.
