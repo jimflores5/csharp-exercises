5e5a9dc098aba26fb5775c1810bfc119b622b7dd
Jim Flores <jimflores5@gmail.com>
2017-11-03 08:36:51 -0500

    Added do/while loop to area of circle program.

--- AreaOfCircle/Program.cs
+++ AreaOfCircle/Program.cs
@@ -9,21 +9,16 @@ namespace AreaOfCircle
             float radius;
             double area;
 
-            //Console.WriteLine("Enter the radius of the circle in cm:");
-            //radius = float.Parse(Console.ReadLine());
-
-            radius = -1;
-            while (radius < 0)
+            do
             {
                 Console.WriteLine("Enter the radius of the circle in cm:");
                 radius = float.Parse(Console.ReadLine());
-                if (radius > 0)
+                if (radius < 0)
                 {
-                    break;
+                    Console.WriteLine("Radius cannot be negative, you FOOL!");
+                    Console.WriteLine();
                 }
-                Console.WriteLine("Radius cannot be negative, you FOOL!");
-                Console.WriteLine();
-            }
+            } while (radius < 0);
             
             area = Math.Round(Math.PI * Math.Pow(radius, 2), 3);
             Console.WriteLine($"The area of the circle is {area.ToString()} cm^2.");
