17805c047b8eb749f64988d1a4f96dcbf64bb87d
Jim Flores <jimflores5@gmail.com>
2017-11-13 08:33:08 -0600

    Modified the CheeseMVC web app for class 3.4. prep work.

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -10,10 +10,28 @@ namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-        // GET: /<controller>/
+        static private Dictionary<string,string> CheeseList = new Dictionary<string,string>();  // 'static' means that the property (CheeseList) is available to all methods inside the CheeseController class.
+
         public IActionResult Index()
         {
+            ViewBag.cheeses = CheeseList;
+
+            return View();  //View() tells the program to search the Views/Cheese and Views/Shared folders for a template called "Index.cshtml".
+                            //By passing in the parameter like View("Error"), the program will search the Views folders for the specified filename.
+        }
+
+        public IActionResult Add()
+        {
+
             return View();
+        }
+
+        [Route("/Cheese/Add")]
+        [HttpPost]
+        public IActionResult NewCheese(string name, string info)
+        {
+            CheeseList.Add(name,info);
+            return Redirect("/Cheese");
         }
     }
 }
--- HelloMVC/Controllers/HelloController.cs
+++ HelloMVC/Controllers/HelloController.cs
@@ -31,21 +31,20 @@ namespace HelloMVC.Controllers
         [Route("/Hello")] //This routes the input from Index() through Display(), but the content appears in the /Hello window rather than /Hello/Display.
         [HttpPost]
         public IActionResult Display(string name, string lang)
-        {                                   //Could replace the if() check by adding a default: Index(string name = "World")
+        {                                   
             var greetings = new Dictionary<string, string>();
             greetings.Add("en", "Hello");
             greetings.Add("fr", "Bonjour");
             greetings.Add("sp", "Hola");
             greetings.Add("gr", "Guten tag");
             greetings.Add("tx", "Howdy");
-
+                                            //Could replace the if() check by adding a default: Index(string name = "World")
             if (string.IsNullOrEmpty(name)) //Checks to is if there is a name parameter (/Hello/Index?name="blah").
             {
-                return Content("<h1>Hello, World!</h1>", "text/html");  //Content is a method defined in IActionResult.
+                return Content("<h1>" + greetings[lang] + ", World!</h1>", "text/html");  //Content is a method defined in IActionResult.
             }
             else
             {
-                
                 return Content(String.Format("<h1>" + greetings[lang] + ", {0}!</h1>", name), "text/html");
             }
         }
